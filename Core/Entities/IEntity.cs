56b462c01f79b10e3db40b01951aedade761631e
Murat GÜL <muratc60@hotmail.com>
2021-02-09 23:30:57 +0300

    Add project files.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    //IEntity imlement eden clas bir veri taban tablosudur
+    public interface IEntity
+    {
+    }
+}
